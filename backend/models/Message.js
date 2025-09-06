ec325afccd3c3840b665aebd4f46a960a9c8ce84
Dipanshu MITTAL <dipanshumittal466@gmail.com>
2025-09-06 10:16:54 +0530

    Delete backend directory

--- backend/models/Message.js
+++
@@ -1,9 +0,0 @@
-
-const mongoose = require('mongoose');
-const MessageSchema = new mongoose.Schema({
-  roomId: String, // jobId or composite
-  from: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  to: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  text: String
-}, { timestamps: true });
-module.exports = mongoose.model('Message', MessageSchema);
